fab7a21cf9c495cfb72bec439337f2bb3c60fcd5
Marko Mucalov <mucalovmarko AT gmail DOT com>
2018-11-04 19:48:06 +0100

    inicijalni projekat

---
+++ NSTrafficAssistant/src/main/java/com/team9/NSTrafficAssistant/NsTrafficAssistantApplication.java
@@ -0,0 +1,12 @@
+package com.team9.NSTrafficAssistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsTrafficAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsTrafficAssistantApplication.class, args);
+	}
+}
