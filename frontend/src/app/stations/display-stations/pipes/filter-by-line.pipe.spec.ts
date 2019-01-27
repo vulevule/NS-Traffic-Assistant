0b324f2eeb3dfb11d539991544f6e775cf630500
Jelena Pejicic <jelenapejicic2008@hotmail.com>
2019-01-27 20:57:57 +0100

    fix

--- frontend/src/app/stations/display-stations/pipes/filter-by-line.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterByLinePipe } from './filter-by-line.pipe';
-
-describe('FilterByLinePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterByLinePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- frontend/src/app/stations/display-stations/pipes/filter-by-type.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterByTypePipe } from './filter-by-type.pipe';
-
-describe('FilterByTypePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterByTypePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
